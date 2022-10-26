d0d91e3e99f398f221458c85d5d9dbad6d9de8b2
NietoMarcelo <nietomarceloalejandro@gmail.com>
2022-10-26 00:44:34 -0300

    Se termina de realizar la maquetacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PayBokWeb';
+
+  hamburguer: string = '';
+  imageLigth: string = '';
+  imageLigthStyle: string = '';
+  showImageStyle: string = '';
+
+  constructor(){
+  }
+
+  changeStyle(value: number){
+    if(this.hamburguer == '' && value == 1){
+      this.hamburguer = 'spread';
+    } else {
+      this.hamburguer = ''
+    }
+  }
+
+  changeImageLight(value: string){
+    this.imageLigth = '../assets/' + value;
+    if(this.imageLigthStyle == ''){
+      this.imageLigthStyle = 'show';
+      this.showImageStyle = 'showImage';
+    } else {
+      this.imageLigthStyle = '';
+      this.showImageStyle = '';
+    }
+    console.log(this.imageLigth);
+  }
+
 }
